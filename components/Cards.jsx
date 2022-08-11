9a7037cbd0d0105f3d49379364173da2685d9a63
yessyprmtsrtmp <91022803+yessyprmtsrmp@users.noreply.github.com>
2022-08-11 21:49:15 +0700

    first commit

---
+++ components/Cards.jsx
@@ -0,0 +1,61 @@
+import Image from "next/image"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { Assets } from "../assets"
+import { getStorage } from "../helper/storage"
+
+const Cards = ({ name, catchPokemon, refresh }) => {
+    const [owended, setOwened] = useState(0)
+
+    useEffect(() => {
+        const catchData  = getStorage("catch")
+        if(catchData !== null) {
+
+            const parseCatch = JSON.parse(catchData)
+            for(const i in parseCatch) {
+                if(parseCatch[i].name === name) {
+                    setOwened(parseCatch[i].owned)
+                }
+            }
+        }
+    }, [name, catchPokemon, refresh])
+
+    return (
+        <div className="flex shadow-sm rounded bg-gray-200 p-4 m-2 w-12/12 md:w-3/12 lg:w-2/12 flex-col">
+            <Link href={{ pathname: "/detail", query : { "name" : name } }}>
+                <a>
+                    <div className="flex p-5">
+                        <Image src={Assets.BallSvg} alt="poke-ball"/>
+                    </div>
+                    <div className="flex justify-center h-full">
+                        <span className="capitalize">{name}</span>
+                    </div>
+                </a>
+            </Link>
+            <div className="flex mt-10 justify-end w-full flex-col h-full">
+                <div className="bg-gray-100 px-5 py-3 rounded-full flex shadow-sm justify-center">
+                    <div className="flex">
+                        <span className="text-black text-sm">Owened :</span>
+                    </div>
+                    <div className="flex ml-3">
+                        <span className="text-black text-sm font-bold">
+                            {owended}
+                        </span>
+                    </div>
+                </div>
+                <div className="bg-gray-100 px-5 py-3 rounded-full flex shadow-sm cursor-pointer mt-2 justify-center" onClick={catchPokemon}>
+                    <div className="flex">
+                        <Image src={Assets.Ball} width={20} height={20} alt="ball" className="shadow-sm"/>
+                    </div>
+                    <div className="flex ml-3">
+                        <span className="text-black text-sm">
+                            Catch !
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
---
+++ pages/index.jsx
@@ -0,0 +1,154 @@
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import { getData } from "../adapter";
+import Cards from "../components/Cards";
+import Search from "../components/Search";
+import { getStorage, setStorage } from "../helper/storage";
+
+const Home = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [catched, setCatched] = useState(false);
+
+  useEffect(() => {
+    randomPokemon();
+  }, []);
+
+ 
+
+
+  const randomPokemon = async () => {
+    let dataRandom = [];
+
+    const dataPokemon = await getData("pokemon", {
+      offset: 0,
+      limit: 1154,
+    });
+
+    for (let i = 0; i < 5; i++) {
+      const randomNumber = Math.floor(Math.random() * 1154);
+      dataRandom.push(dataPokemon.data.results[randomNumber]);
+    }
+
+    setPokemon(dataRandom);
+  };
+
+  const searchPokemon = async (keyword) => {
+    let dataSearch = [];
+    const dataPokemon = await getData("pokemon", {
+      offset: 0,
+      limit: 1154,
+    });
+
+    for (const i in dataPokemon.data.results) {
+      if (dataPokemon.data.results[i].name.indexOf(keyword.toLowerCase()) !== -1) {
+        dataSearch.push(dataPokemon.data.results[i]);
+      }
+	  console.log(dataSearch);
+    }
+
+    setPokemon(dataSearch);
+  };
+
+  const catchPokemon = (value) => {
+    let dataArray = [];
+    const dataCatch = getStorage("catch");
+
+    if (dataCatch === null) {
+      dataArray.push({
+        name: value.name,
+        owned: 1,
+      });
+
+      const stringyfyCatch = JSON.stringify(dataArray);
+      setStorage("catch", stringyfyCatch);
+      setCatched(true);
+      Swal.fire({
+        title: "Success Catch",
+        text: `Congratulation you catch ${value.name} !`,
+        icon: "success",
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "OK",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          setCatched(false);
+        }
+      });
+    } else {
+      let totalCatch = 1;
+      const parseCatch = JSON.parse(dataCatch);
+
+      // count
+      for (const l in parseCatch) {
+        totalCatch += parseCatch[l].owned;
+      }
+
+      if (totalCatch <= 10) {
+        let sameName = [];
+        for (const i in parseCatch) {
+          if (parseCatch[i].name === value.name) {
+            sameName.push(parseCatch[i]);
+          }
+        }
+
+        if (sameName.length > 0) {
+          for (const p in sameName) {
+            for (const i in parseCatch) {
+              if (sameName[p].name === parseCatch[i].name) {
+                parseCatch[i].owned = parseInt(parseCatch[i].owned) + 1;
+              }
+            }
+          }
+        } else {
+          dataArray.push({
+            name: value.name,
+            owned: 1,
+          });
+        }
+
+        setStorage("catch", JSON.stringify([...parseCatch, ...dataArray]));
+        setCatched(true);
+        Swal.fire({
+          title: "Success Catch",
+          text: `Congratulation you catch ${value.name} !`,
+          icon: "success",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "OK",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            setCatched(false);
+          }
+        });
+      } else {
+        Swal.fire({
+          title: "Bag Full",
+          text: "Sorry your bag is full, release pokemon !",
+          icon: "warning",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "OK",
+        });
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col mt-10">
+      <div className="flex mx-2 justify-center">
+        <Search click={searchPokemon} generate={randomPokemon} />
+      </div>
+      <div className="flex mt-10 justify-center flex-wrap">
+        {pokemon.map((poke, i) => {
+          return (
+            <Cards
+              name={poke.name}
+              key={i}
+              catchPokemon={() => catchPokemon(poke)}
+              refresh={catched}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
